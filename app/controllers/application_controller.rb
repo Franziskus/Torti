0af71fb8cd8b8785f45dfd36c25d9a2e45f2026e
Franz <Franz@Navi>
2013-06-23 21:52:49 +0200

    Chaned login so now there is a simpel method in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  public
+  def getUser(currentAction)
+    if(session[:userId] == nil)
+      session[:beforeLogin] = currentAction;
+      redirect_to :login_login;
+    else
+      return RegisterdUser.find_by_id(session[:userId]);
+    end
+  end
 end
